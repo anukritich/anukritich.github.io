a00a917c8fee2c39def608e26d67c99df339aff8
Anukriti Choudhary <136055035+anukritich@users.noreply.github.com>
2025-01-07 10:29:48 +0530

    Update model.js

--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,5 @@
-import * as THREE from 'three';
+//import * as THREE from 'three';
+import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
