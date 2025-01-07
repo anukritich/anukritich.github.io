b1e6240746754572a12076adad4edd0666003b57
Anukriti Choudhary <136055035+anukritich@users.noreply.github.com>
2025-01-07 10:36:11 +0530

    Update model.js

--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,5 @@
-//import * as THREE from 'three';
+import * as THREE from 'three';
+import * as THREE from './js/three.module.js';
 import * as THREE from "../node_modules/three/build/three.module.js";
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
