3d3e3250be7faba2cd29ac57890c8a90f5b13c6c
Anukriti Choudhary <136055035+anukritich@users.noreply.github.com>
2025-01-10 14:37:23 +0530

    Update model.js

--- src/js/model.js
+++ src/js/model.js
@@ -1,6 +1,6 @@
-import * as THREE from 'three';
-import * as THREE from './js/three.module.js';
-import * as THREE from "../node_modules/three/build/three.module.js";
+//import * as THREE from 'three';
+//import * as THREE from './js/three.module.js';
+//import * as THREE from "../node_modules/three/build/three.module.js";
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
