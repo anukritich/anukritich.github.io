79fe3016b83ca11e76dce1046494c5ea98219836
anukritich <chyanukriti@gmail.com>
2025-01-29 11:28:42 +0530

    Updates

--- assets/index-BUKQ_AfE.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";const e=document.querySelector(".sidebar"),r=document.querySelector(".sidebar-toggler");r.addEventListener("click",()=>{e.classList.toggle("collapsed")});
